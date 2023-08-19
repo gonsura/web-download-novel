0aaf7a47502bc42182de3e09176a11c15fd7af4c
gonsura <iden.cioci@gmail.com>
2023-08-19 21:59:49 +0800

    modify db

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
-
 import { port } from './utils/envCheck'
 
 const app = express()
 
+
+
+
 app.get('/', (_req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}`)
 })
